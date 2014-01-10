b4ada9456fa57e4ca91d268ae45b34d5c9ddddb2
Erich Rahm <erahm84@gmail.com>
2014-01-09 23:22:23 -0700

    api changes

--- config/ApiKeys.php
+++ config/ApiKeys.php
@@ -4,8 +4,8 @@ namespace config;
 
 class ApiKeys {
 
-    private  $apiKey = 'ad0d52d91479ad00cc66d4103d63a53f';
-    private  $secret = '43997ac3f6373cd44ea0952181d8587931f289fbac5940bf2b9cfcb8ec8b8064';
+    private  $apiKey = 'insert your api key here';
+    private  $secret = 'insert your secret key here';
 
     /**
      * @return string
