ade6fd5ddf982b31cd68a29bd0a78daef506f3cf
Erich Rahm <darkstar1984@gmail.com>
2014-01-10 17:52:29 +0000

    Actually made the logic changes to BoardPreferences.php this time

--- library/BoardPreferences.php
+++ library/BoardPreferences.php
@@ -21,8 +21,28 @@ class BoardPreferences {
     protected $canInvite;
 
     public function populate(array $prefs) {
+    $boolArray = array('canBePublic', 'canBeOrg', 'canBePrivate', 'canInvite');
+    
       foreach ($prefs as $key => $value) {
+        if (in_array($key, $boolArray)) {
+          $this->setBoolean($key, $value);
+        }
+        else {
           $this->$key = $value;
+        }
+      }
+    }
+    
+    protected function setBoolean($key, $value) {
+      swich(strtolower($value) {
+        case 'true':
+          $this->$key = true;
+          break;
+        case 'false':
+          $this->$key = false;
+          break;
+        default:
+          break;
       }
     }
 
